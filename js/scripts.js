c9eb3d1e40c20ba8db352600a23637b7950b8978
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-02-19 22:06:47 -0800

    start adding rain

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,6 @@ $(function(){
       $(this).css('width', Math.floor(Math.random() * (40-20) + 20));
       $(this).css('height', Math.floor(Math.random() * (40-20) + 20));
     })
-
     $('.mids').each(function(idx){
       $(this).css('right', idx * 22);
       $(this).css('width', Math.floor(Math.random() * (40-20) + 20));
@@ -18,16 +17,21 @@ $(function(){
       $(this).css('width', Math.floor(Math.random() * (40-20) + 20));
       $(this).css('height', Math.floor(Math.random() * (70-30) + 20));
     })
+    // $('#rain').append('<li>')
     $('#cloud').show();
+    $('#rain').show();
     $('#miniGame').addClass('noCursor');
 
   }, function() {
     $('#cloud').hide();
+    $('#rain').hide();
   } );
 
   $('#miniGame').mousemove(function(cloud) {
     $('#cloud').css({
       left: cloud.pageX, top: cloud.pageY });
+    $('#rain').css({
+      left: cloud.pageX, top: cloud.pageY + 100 });
   });
 
 
