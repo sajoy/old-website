6816d1ef336e811bdf3f2d0fcc3317a910082c65
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-02-19 21:47:16 -0800

    it will form a random cloud on each hover

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
 $(function(){
 
+
   $('#miniGame').hover(function() {
+    $('.bottoms').each(function(idx){
+      $(this).css('right', idx * 20);
+      $(this).css('width', Math.floor(Math.random() * (40-20) + 20));
+      $(this).css('height', Math.floor(Math.random() * (40-20) + 20));
+    })
+
+    $('.mids').each(function(idx){
+      $(this).css('right', idx * 22);
+      $(this).css('width', Math.floor(Math.random() * (40-20) + 20));
+      $(this).css('height', Math.floor(Math.random() * (60-30) + 20));
+    })
+    $('.tops').each(function(idx){
+      $(this).css('right', idx * 20);
+      $(this).css('width', Math.floor(Math.random() * (40-20) + 20));
+      $(this).css('height', Math.floor(Math.random() * (70-30) + 20));
+    })
     $('#cloud').show();
     $('#miniGame').addClass('noCursor');
 
@@ -10,8 +27,7 @@ $(function(){
 
   $('#miniGame').mousemove(function(cloud) {
     $('#cloud').css({
-      left: cloud.pageX, top: cloud.pageY + 10
-    });
+      left: cloud.pageX, top: cloud.pageY });
   });
 
 
