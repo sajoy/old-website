21b1b03fdfc5dab8f5c809b1520119e4c0012690
Sarah Joy Calpo <sajoy.pc@gmail.com>
2016-03-21 18:30:03 -0700

    add gratitude popup

--- gratitude.js
+++ gratitude.js
@@ -1,17 +1,19 @@
-function attach () {
-    document.getElementById('thanks').addEventListener( 'click', function () {
-        alert( 'clicked!' );
-        // add new section:
-        // translucent overlay
-        // text: seriously, you're awesome
-        // text: pull from array,  reminders- you're awesome, etc
-        // how to close it??
+window.onload = function () {
+    document.getElementById( 'thanks' ).addEventListener( 'click', () => {
+        var container = document.getElementById( 'gratitude' ),
+            reminders = ['you rock.', 'call your loved ones.', 'keep your head up.', 'deep breath, relax.', 'everything is temporary.', 'your smile is awesome.', 'failing > not trying.'],
+            colors = [ 'rgba( 280, 0, 100, .8 )', 'rgba( 280, 200, 10, .8 )', 'rgba( 60, 240, 70, .8 )', 'rgba( 0, 100, 200, .8 )', 'rgba( 200, 90, 200, .8 )' ],
+            color = colors[ Math.floor( Math.random(1) * 4 ) ];
 
-        //or
+        container.classList.add( 'showit' );
+        container.style.backgroundColor = color;
+        setTimeout( () => { container.style.opacity = 1; } );
 
-        // rain hearts
+        document.getElementById( 'reminder' ).innerHTML = reminders[ Math.floor( Math.random(1) * 7 ) ];
 
+        document.getElementById( 'close' ).addEventListener( 'click', () => {
+            container.style.opacity = 0;
+            setTimeout( () => { container.classList.remove( 'showit' ); }, 500 );
+        });
     });
 }
-
-window.onload = attach;
