abdbb0f374440f2cea9bd5580cce955708a23828
Sarah Joy Calpo <sajoy.pc@gmail.com>
2016-03-31 22:14:54 -0700

    add favicon

--- gratitude.js
+++ gratitude.js
@@ -1,12 +1,11 @@
 window.onload = function () {
-    document.getElementById( 'thanks' ).addEventListener( 'click', () => {
-        var container = document.getElementById( 'gratitude' ),
-            reminders = ['you rock.', 'call your loved ones.', 'keep your chin up.', 'deep breath, relax.', 'everything is temporary.', 'your smile is awesome.', 'failing > not trying.'],
-            colors = [ 'rgba( 280, 0, 100, .8 )', 'rgba( 280, 200, 10, .8 )', 'rgba( 60, 240, 70, .8 )', 'rgba( 0, 100, 200, .8 )', 'rgba( 200, 90, 200, .8 )' ],
-            color = colors[ Math.floor( Math.random(1) * 4 ) ];
+    var container = document.getElementById( 'gratitude' ),
+    reminders = ['you rock.', 'call your loved ones.', 'keep your chin up.', 'deep breath, relax.', 'everything is temporary.', 'your smile is awesome.', 'failing > not trying.'],
+    colors = [ 'rgba( 280, 0, 100, .8 )', 'rgba( 280, 200, 10, .8 )', 'rgba( 60, 240, 70, .8 )', 'rgba( 0, 100, 200, .8 )', 'rgba( 200, 90, 200, .8 )' ];
 
+    document.getElementById( 'thanks' ).addEventListener( 'click', () => {
         container.classList.add( 'showit' );
-        container.style.backgroundColor = color;
+        container.style.backgroundColor = colors[ Math.floor( Math.random(1) * 4 ) ];
         setTimeout( () => { container.style.opacity = 1; } );
 
         document.getElementById( 'reminder' ).innerHTML = reminders[ Math.floor( Math.random(1) * 7 ) ];
@@ -15,5 +14,6 @@ window.onload = function () {
             container.style.opacity = 0;
             setTimeout( () => { container.classList.remove( 'showit' ); }, 500 );
         });
+
     });
 }
