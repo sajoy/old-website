1337827953c16ffbfd43a570805584c848bb08cb
Sarah Joy Calpo <sajoy.pc@gmail.com>
2017-10-10 19:10:46 -0700

    position magnets in rows

--- js/magnets.js
+++ js/magnets.js
@@ -5,38 +5,59 @@ const Dragg = Draggable.Draggable;
 const quote = [
     `but`, `if`, `poetry`, `is`, `your`, `goal`, `, `, `you've`, `got`, `to`, `forget`, `all`, `about`, `punishments`, `and`, `all`, `about`, `rewards`, `and`, 
     `all`, `about`, `selfstyled`, `obligations`, `and`, `duties`, `and`, `responsibilities`, `etcetera`, `ad`, `infinitum`, `and`, `remember`, `one`, `thing`, `only`, `:`, 
-    `that`, `it's`, `you`, `&mdash;`, `nobody`, `else`, `&mdash;`, `who`, `determine`, `your`, `destiny`, `and`, `decide`, `your`, `fate`, 
+    `that`, `it's`, `you`, `—`, `nobody`, `else`, `—`, `who`, `determine`, `your`, `destiny`, `and`, `decide`, `your`, `fate`, 
     `.`, `nobody`, `else`, `can`, `be`, `alive`, `for`, `you`, `;`, `nor`, `can`, `you`, `be`, `alive`, `for`, `anybody`, `else`, `.`, `Toms`, `can`, `be`, `Dicks`, `and`, 
     `Dicks`, `can`, `be`, `Harrys`, `, `, `but`, `none`, `of`, `them`, `can`, `ever`, 
     `be`, `you`, `.`, `there's`, `the`, `artist's`, `responsibility`, `;`, `and`, `the`, `most`, `awful`, 
-    `responsibility`, `on`, `earth`, `.`, `if`, `you`, `can`, `take`, `it`, `, `, `take`, `it`, `&mdash;`, `and`, `be`, `.`, 
+    `responsibility`, `on`, `earth`, `.`, `if`, `you`, `can`, `take`, `it`, `, `, `take`, `it`, `—`, `and`, `be`, `.`, 
     `if`, `you`, `can't`, `, `, `cheer`, `up`, `, `, `and`, `go`, `about`, `other`, `people's`, `business`, `;`, `and`, `do`, `(`, 
     `or`,  `undo`,  `)`,  `till`,  `you`,  `drop`,  `.`,  `-`,  `e`,  `.`,  `e`,  `.`,  `cummings` 
 ];
 
-quote.map( content => {
+console.log( 'l', quote.length);
+const magnetEles = quote.map( content => {
     let magnet = document.createElement( 'p' );
     magnet.classList.add( 'magnet' ); 
     magnet.innerText = content;
     return magnet;
-}).forEach( (magnet, i, eles) => {
-    let prevLeft, prevWidth;
-
-    if ( i === 0 ) {
-        prevLeft = 0;
-        prevWidth = 0;
-    } else {
-        let prev = eles[i - 1];
-        prevLeft = parseInt(prev.style.left);
-        prevWidth = prev.getBoundingClientRect().width + 8;
-    }
-
-    magnet.style.left = `${prevLeft + prevWidth}px`;
-
-
-
-    document.querySelector( 'main' ).appendChild( magnet );
 });
+
+const groups = [];
+while ( magnetEles.length > 0 ) {
+    groups.push( magnetEles.splice(0,16) );
+}
+
+groups.forEach( (group, i) => { // iterates over 12 groups
+    let top = (i * 60) + 30;
+
+    group.forEach( (magnet, j, eles) => {
+        console.log( `inside group ${i}`, magnet );
+        let prevLeft, prevWidth;
+        
+        console.log( 'j is', j );
+        if ( j === 0 ) {
+            prevLeft = 200;
+            prevWidth = 0;
+        } else {
+            let prev = eles[j - 1];
+            let prevDimensions = prev.getBoundingClientRect();
+            prevLeft = parseInt(prev.style.left);
+            prevWidth = prevDimensions.width + 8;   
+        }
+        
+        magnet.style.left = `${prevLeft + prevWidth}px`;
+        magnet.style.top = `${top}px`;
+        
+        document.querySelector( 'main' ).appendChild( magnet );
+    });
+});
+
+/*
+    strings of words
+    elements
+    arrays of 12 words
+*/
+
 
 new Dragg( document.querySelectorAll( 'main' ), { draggable: '.magnet' } )
     .on( 'drag:start', () => console.log( 'start' ) )
