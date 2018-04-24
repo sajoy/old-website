5aeb4210f8a9bbda7491b1f57bb81ce3153752af
Sarah Joy Calpo <sajoy.pc@gmail.com>
2018-04-23 21:06:44 -0700

    randomize list item decor

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+Array.prototype.getRandom = function () {
+    return this[Math.floor(Math.random() * this.length)];
+};
+
+
 // using anime.js
 // from docs: http://animejs.com/documentation/#morphing
 
@@ -58,36 +63,59 @@ const morphing = anime({
     duration: 8000,
     loop: true,
     autoplay: false
-  });
+});
 
-  const options = document.querySelectorAll('input[type="checkbox"]');
-  options.forEach( btn => {
-      btn.addEventListener('click', function () {
-        switch (this.id) {
-            case 'morph-logo':
-                if (morphing.paused) {
-                    morphing.play();
-                } else {
-                    morphing.reset();
-                    morphing.pause();
-                }
-                break;
-            case 'change-color':
-                document.body.classList.toggle('new-color');
-                
-                const root = document.documentElement;
-                const color = getComputedStyle(root).getPropertyValue("--text-color");
-                root.style.setProperty('--text-color', color !== 'white' ? 'white' : 'rgb(37, 37, 37)');
-                
-                const square = document.querySelector('svg .morph');
-                const stroke = square.getAttribute('stroke');
-                square.setAttribute('stroke', stroke === '#000' ? '#FFF' : '#000');
+const options = document.querySelectorAll('input[type="checkbox"]');
+options.forEach( btn => {
+    btn.addEventListener('click', function () {
+    switch (this.id) {
+        case 'morph-logo':
+            if (morphing.paused) {
+                morphing.play();
+            } else {
+                morphing.reset();
+                morphing.pause();
+            }
+            break;
+        case 'change-color':
+            document.body.classList.toggle('new-color');
+            
+            const root = document.documentElement;
+            const color = getComputedStyle(root).getPropertyValue("--text-color");
+            root.style.setProperty('--text-color', color !== 'white' ? 'white' : 'rgb(37, 37, 37)');
+            
+            const square = document.querySelector('svg .morph');
+            const stroke = square.getAttribute('stroke');
+            square.setAttribute('stroke', stroke === '#000' ? '#FFF' : '#000');
 
-                break;
-            case 'cats':
-                const midnight = document.querySelector('#logo img');
-                midnight.classList.toggle('yawn');
-                break;
-        }
-      });
-  })
+            break;
+        case 'cats':
+            const midnight = document.querySelector('#logo img');
+            midnight.classList.toggle('yawn');
+            break;
+    }
+    });
+});
+
+const toDecorate = document.querySelectorAll('.random-decor li');
+const classes = [
+    {used: 0, class: 'left-stars'},
+    {used: 0, class: 'left-colons'},
+    {used: 0, class: 'left-under'},
+    {used: 0, class: 'right-colons'},
+    {used: 0, class: 'right-arrows'},
+    {used: 0, class: 'squiggles'},
+    {used: 0, class: 'colons'},
+    {used: 0, class: 'slashes'},
+    {used: 0, class: 'equals'},
+    {used: 0, class: 'squares'}
+];
+let uses = 0; 
+
+toDecorate.forEach((li, i) => {
+    const randomClass = classes.filter(item => item.used <= uses).getRandom();
+    randomClass.used++;
+    li.classList.add(randomClass.class);
+
+    if (i % classes.length === 3) uses++;
+});
